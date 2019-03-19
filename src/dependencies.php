a87920ea5b1fe65779e3ff7d15aeba7a214b9676
sbuberl <sbuberl@gmail.com>
2019-03-18 20:43:22 -0400

    Adds checking of the posts content to in list test

--- src/dependencies.php
+++ src/dependencies.php
@@ -18,6 +18,11 @@ $container['logger'] = function ($c) {
     return $logger;
 };
 
+function convertToLocal($utcDate) {
+    $time = strtotime($utcDate);
+    return date("Y-m-d H:i:s", $time);
+}
+
 $container['db'] = function ($c) {
     $db = $c->get('settings')['db'];
     $fsql = new FSQL\Environment();
@@ -33,19 +38,19 @@ $container['db'] = function ($c) {
     $insert = $fsql->prepare("INSERT INTO posts(title, createdTime, content) VALUES (?, ?, ?);");
 
     $title = "Welcome to my blog";
-    $createdTime = "2018-11-26T02:52:40+00:00";
+    $createdTime = convertToLocal("2018-11-26T02:52:40+00:00");
     $content = "Thanks for joining me on my new blog.\nI primarily use it for site news and updates (such as new release or new content added).\nI’m playing around with moving the fSQL docs back to the new site layout now that I’ve gotten rid of Jekyll.";
 
     $insert->bind_param("sss", $title, $createdTime, $content);
     $insert->execute();
 
     $title = "Old Design Retired";
-    $createdTime = "2019-03-08T21:01:46+00:00";
+    $createdTime = convertToLocal("2019-03-08T21:01:46+00:00");
     $content = "I just switched from my sbuberl.com’s old Jekyll-based web design to the current design I was playing around with for a little while in a separate GitHub repo.\nPlus I can customize the syntax highlighting to any style I wish now.";
     $insert->execute();
 
     $title = "Adding Photo Albums";
-    $createdTime = "2019-03-10T20:53:07+00:00";
+    $createdTime = convertToLocal("2019-03-10T20:53:07+00:00");
     $content = "I started added some old photo albums of previous trips and activities to the site.\nI will be posting more soon.";
     $insert->execute();
 
--- tests/Functional/PostsTest.php
+++ tests/Functional/PostsTest.php
@@ -13,8 +13,29 @@ class PostsTest extends BaseTestCase
 
         $body = (string)$response->getBody();
         $posts = json_decode($body);
+        $expected = [
+            (object) [
+                'id' => 1,
+                'title' => 'Welcome to my blog',
+                'createdTime' => '2018-11-26 02:52:40',
+                'content' => "Thanks for joining me on my new blog.\nI primarily use it for site news and updates (such as new release or new content added).\nI’m playing around with moving the fSQL docs back to the new site layout now that I’ve gotten rid of Jekyll.",
+            ],
+            (object) [
+                'id' => 2,
+                'title' => 'Old Design Retired',
+                'createdTime' => '2019-03-08 21:01:46',
+                'content' => "I just switched from my sbuberl.com’s old Jekyll-based web design to the current design I was playing around with for a little while in a separate GitHub repo.\nPlus I can customize the syntax highlighting to any style I wish now.",
+                ],
+            (object) [
+                'id' => 3,
+                'title' => 'Adding Photo Albums',
+                'createdTime' => '2019-03-10 20:53:07',
+                'content' => "I started added some old photo albums of previous trips and activities to the site.\nI will be posting more soon.",
+            ]
+        ];
+
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(3, count($posts));
+        $this->assertEquals($expected, $posts);
     }
 
 }
